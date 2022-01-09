458e30989af6bcc0de0fc6cf01981138b39372df
Andrei Xavier <andrei_xavier@outlook.com>
2022-01-09 11:32:06 -0300

    alteração da migration para tabela categorias

---
+++ IBooksManagerLibrary/Migrations/202201080250075_criacao_tabela_categorias.cs
@@ -0,0 +1,46 @@
+﻿namespace IBooksManagerLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class criacao_tabela_categorias : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categorias",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.LivroCategorias",
+                c => new
+                    {
+                        Livro_Id = c.Int(nullable: false),
+                        Categoria_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.Livro_Id, t.Categoria_Id })
+                .ForeignKey("dbo.Livros", t => t.Livro_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Categorias", t => t.Categoria_Id, cascadeDelete: true)
+                .Index(t => t.Livro_Id)
+                .Index(t => t.Categoria_Id);
+            
+            DropColumn("dbo.Livros", "Genero");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Livros", "Genero", c => c.String());
+            DropForeignKey("dbo.LivroCategorias", "Categoria_Id", "dbo.Categorias");
+            DropForeignKey("dbo.LivroCategorias", "Livro_Id", "dbo.Livros");
+            DropIndex("dbo.LivroCategorias", new[] { "Categoria_Id" });
+            DropIndex("dbo.LivroCategorias", new[] { "Livro_Id" });
+            DropTable("dbo.LivroCategorias");
+            DropTable("dbo.Categorias");
+        }
+    }
+}
